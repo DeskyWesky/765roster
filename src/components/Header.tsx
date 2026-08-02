78411a3d9c642d05fdb0e8fe85b149b124fec7e4
DeskyWesky <givonnisardina12@gmail.com>
2026-08-02 17:54:42 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,7 @@ export default function Header() {
     <header className="roster-header">
       <h2 className="hero-subtitle">765 ROSTER</h2>
       <p className="hero-tagline">
-        a completely unnecessary roster for a group of people who talk every
-        day anyway
+        newest and most harmful gen of 765 ever | dm @deskcpp to be added
       </p>
     </header>
   );
