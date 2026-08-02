78c8c1702d051c51a3fc81d4550e218f6b75fb07
DeskyWesky <givonnisardina12@gmail.com>
2026-08-02 17:20:35 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,7 @@ import AsciiTitle from "./AsciiTitle";
 export default function Header() {
   return (
     <header className="roster-header">
-      <AsciiTitle text="765" className="ascii-title" />
-      <h2 className="hero-subtitle">ROSTER</h2>
+      <h2 className="hero-subtitle">765 ROSTER</h2>
       <p className="hero-tagline">
         a completely unnecessary roster for a group of people who talk every
         day anyway
